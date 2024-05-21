73990b472cc949b24b21efd21ee72c553431bf3c
Kamil Banach <banachkamil@outlook.com>
2024-05-21 19:35:22 +0200

    Rabbit CONNECTION delay

--- HotelService/HotelQueueHandler.cs
+++ HotelService/HotelQueueHandler.cs
@@ -68,6 +68,7 @@ public class HotelQueueHandler : IDisposable
             {
                 _logger.Error("{p}Couldn't connect to the RabbitMq server, trying again. Check connection string and/or connection {e}",
                     LoggerPrefix, e);
+                Task.Delay(100).Wait();
             }
         }
         
